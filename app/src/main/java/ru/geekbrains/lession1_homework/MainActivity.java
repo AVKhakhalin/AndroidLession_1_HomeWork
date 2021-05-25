5a0b68309b17007f51a2c6a58b820126e50f56b6
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-05-25 12:53:09 +0300

    Correct buttons and created switch between layouts.

--- app/src/main/java/ru/geekbrains/lession1_homework/MainActivity.java
+++ app/src/main/java/ru/geekbrains/lession1_homework/MainActivity.java
@@ -3,14 +3,28 @@ package ru.geekbrains.lession1_homework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.task2_maket);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.task2_maket);
 //        setContentView(R.layout.task3_calendarview);
+    }
+
+    public void switchLayout1(View view)
+    {
+        setContentView(R.layout.activity_main);
+    }
+    public void switchLayout2(View view)
+    {
+        setContentView(R.layout.task2_maket);
+    }
+    public void switchLayout3(View view)
+    {
+        setContentView(R.layout.task3_calendarview);
     }
 }
